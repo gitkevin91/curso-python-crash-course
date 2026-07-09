1839a8b7fb61fea871c05a1c2c73bb6d510aacba
kevin garcia <kevin.garcia.an.098@gmail.com>
2026-07-08 22:41:28 -0300

    probando metodos de manipulacion de arrays:append,insert,pop.remove,sorted,reverse,sort.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ apellido= "lovelace"
 nombreCompleto=f"{nombre}{apellido}"
 print(f"hola,{nombreCompleto.title()}!")
 prefijos="https//:www.argonauta.com.ar"
-prefijos.removeprefix('https//:')
+print(prefijos.removeprefix('https//:'))
 nombre2="kevin"
 
 apellido2="garcia"
@@ -36,7 +36,7 @@ print(f"tengo muy buenos recuerdos con {amigos[3].title()}")
 print(f"tengo muy buenos recuerdos con {amigos[2].title()}")
 print(f"tengo muy buenos recuerdos con {amigos[1].title()}")
 print(f"tengo muy buenos recuerdos con {amigos[0].title()}")
-motos=["yamaha r6","d"]
+motos=["yamaha r6","d","a","g","c","b"]
 amigos[3]="almendra"
 amigos.append("calidoscopio")#con este metodo insertamos un elemento al final de la lista
 print(amigos)
@@ -47,3 +47,49 @@ print (amigos)
 examigos=amigos.pop()#pop se usa para eliminar el ultimo indice y a su vez guardar el valor en otra variable
 print (examigos)#aca va aparecer guada ya que era el ultimo valor en la lista
 print (amigos)
+futuraMoto="yamaha r6"
+motos.remove(futuraMoto)#de esta manera lo que hacemos es que una variable con un dato especifico que coincida con uno de la lista sea eliminado
+print(f"en dos años mi moto va ser la {futuraMoto.title()}")
+ciudades=["morocco","london","ibiza","new york","madrid","beijing","tokio","roma"]
+print(f"  {ciudades}")
+
+
+sinvisa=ciudades.pop()
+print(f"no pude entrar en {sinvisa} por que no tengo visa")
+ciudades.append("kioto")
+print(ciudades[7])
+ciudades.insert(8,"managua")
+print(ciudades[8])
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+sinvisa=ciudades.pop()
+print(f"no voy a poder ir a{sinvisa}")
+
+print(f"felicidades, vas a viajar a:{ciudades[0]}")
+print(f"felicidades, vas a viajar a:{ciudades[1]}")
+del(ciudades)
+
+
+print(sorted(motos))#el metodo sorted ordena la lista igual que sort pero de manera temporal
+print(motos)
+lugares=["japon","vietnam","rumania","irlanda","españa"]
+print(sorted(lugares))
+print(lugares)
+print(sorted(lugares,reverse=True))
+print(lugares)
+lugares.reverse()#el metodo reverse invierte el ordenamiento del array
+print(lugares)
+lugares.reverse()
+print(lugares)
+lugares.sort()
+print(lugares)
+lugares.sort(reverse=True)
+print(lugares)
